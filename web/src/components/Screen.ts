2e6e5bb2fc5e847c58fb688e4e3a378f0b4c74ca
Andrej Germič <andrej.germic@moderntv.eu>
2022-02-23 02:11:21 +0100

    feat(components): added screen component

---
+++ web/src/components/Screen.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+const Screen = styled.div`
+  background: ${({ theme }) => theme.colors.bgPrimary};
+  width: 100%;
+  height: 100vh;
+  position: relative;
+`
+
+export { Screen }
