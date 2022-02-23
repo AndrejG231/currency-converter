17d63060ffd953b91b5c4a40fddee03da893ad99
Andrej Germič <andrej.germic@moderntv.eu>
2022-02-23 11:30:48 +0100

    feat(components): added fallback component

---
+++ web/src/components/Fallback.tsx
@@ -0,0 +1,26 @@
+import { FC } from "react"
+import styled from "styled-components"
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const FallbackText = styled.span`
+  display: block;
+  color: ${({ theme }) => theme.colors.white};
+  font-size: 2rem;
+`
+
+const Fallback: FC = ({ children }) => {
+  return (
+    <Wrapper>
+      <FallbackText>Loading...</FallbackText>
+    </Wrapper>
+  )
+}
+
+export { Fallback }
