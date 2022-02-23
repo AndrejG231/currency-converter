424279a4095181a367e8162b70131c35264bf12b
Andrej Germič <andrej.germic@moderntv.eu>
2022-02-23 01:50:51 +0100

    feat(client): added styled-components theme

--- web/src/core/App.tsx
+++ web/src/core/App.tsx
@@ -1,12 +1,14 @@
+import { ThemeProvider } from "styled-components"
 import { GlobalStyle } from "./GlobalStyle"
 import { Router } from "./Routes"
+import { theme } from "./theme"
 
 const App = () => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Router />
-    </>
+    </ThemeProvider>
   )
 }
 
---
+++ web/src/core/theme.ts
@@ -0,0 +1,11 @@
+import { DefaultTheme } from "styled-components"
+
+const theme: DefaultTheme = {
+  colors: {
+    bgPrimary: "#0C081B",
+    bgSecondary: "#3666AE",
+    white: "#FFFFF",
+  },
+}
+
+export { theme }
